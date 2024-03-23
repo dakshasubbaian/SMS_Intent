c739d381b676fae9cfd49d968657dab4ee80fe6b
dakshasubbaian <112880924+dakshasubbaian@users.noreply.github.com>
2024-03-23 10:47:24 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,31 +1,32 @@
-package com.example.ex_5;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.myapplication;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
+import android.view.View;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         Button mbutton=(Button) findViewById(R.id.smsButton);
         mbutton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) 
-          {
-                /*.................WRITE THE CODING PART FOR INTENT METHOD FOR SENDING SMS..............
-                ........................................................................................
-                ........................................................................................
-               .........................................................................................
-               */
+            public void onClick(View view) {
+                Intent intent =new Intent(Intent.ACTION_VIEW, Uri.fromParts("sms","9841680383",null));
+                intent.putExtra("sms_body","SMS using Intent");
+                startActivity(intent);
             }
         });
 
